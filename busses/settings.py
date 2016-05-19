6390ea6be2c9512a2ccfc4e1ef2ce81de78d9176
mestrebisli <mestrebisli@gmail.com>
2016-05-19 23:05:15 +0300

    moved data folder

--- busses/settings.py
+++ busses/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -27,7 +26,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-BUS_DATA_DIR = "../data"
+BUS_DATA_DIR = BASE_DIR+"/data"
 
 
 # Application definition
